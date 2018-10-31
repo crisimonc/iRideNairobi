622f8a8b516a85e899c152e25a0cc15edfdc3754
Alessandra Cestaro <alessandra_cestaro@mckinsey.com>
2018-10-31 15:34:49 +0100

    Seeds WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "Cleaning DB..."
+Motorcycle.destroy_all
+
+
+puts "Creating motorcycles..."
+motorcycles_attributes = [
+  {
+    name: "",
+    type: "",
+    description: "",
+    price: ,
+    picture: "",
+  }
+]
+
+Motorcycle.create!(motorcycles_attributes)
+puts "Motorcycles created!"
+
+# create_table "motorcycles", force: :cascade do |t|
+#   t.string "name"
+#   t.string "type"
+#   t.text "description"
+#   t.integer "price"
+#   t.string "picture"
