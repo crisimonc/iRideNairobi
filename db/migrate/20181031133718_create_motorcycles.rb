3db600306c73e8e42d2b2d54f3194953e1637343
Cristina Simoncini <crisimonc@gmail.com>
2018-10-31 15:30:04 +0100

    type changed to motor_type

--- db/migrate/20181031133718_create_motorcycles.rb
+++ db/migrate/20181031133718_create_motorcycles.rb
@@ -2,7 +2,7 @@ class CreateMotorcycles < ActiveRecord::Migration[5.2]
   def change
     create_table :motorcycles do |t|
       t.string :name
-      t.string :type
+      t.string :motor_type
       t.text :description
       t.integer :price
       t.string :picture
