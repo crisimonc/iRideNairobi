0b430a9fd07e1b67036ace4e5665343e7e52b9d7
Anders Fleck <andersfleck@gmail.com>
2018-10-31 15:32:12 +0100

    please add my changes

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -1,2 +1,12 @@
 class MotorcyclesController < ApplicationController
+
+  def new
+    @motorcycle = Motorcycle.new
+  end
+
+  def create
+    @motorcycle = Motorcycle.new(params[:name :type :description]
+    @motorcycle.save
+  end
+
 end
