0ae6448f95257c76e73b71dbf24252cb811dd734
Alessandra Cestaro <alessandra_cestaro@mckinsey.com>
2018-11-01 12:04:04 +0100

    WIP Bookings new and create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,33 @@
 class BookingsController < ApplicationController
+  def new
+    @motorcycle = Motorcycle.find(params[:motorcycle_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    @booking.motorcycle = Motorcycle.find(params[:motorcycle_id])
+    @booking.save
+
+    redirect_to motorcycle_bookings_path(@booking.motorcycle)
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :motorcycle_id, :current_user)
+  end
+
 end
+
+# create_table "bookings", force: :cascade do |t|
+#     t.date "start_date"
+#     t.date "end_date"
+#     t.bigint "motorcycle_id"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.bigint "user_id"
+#     t.index ["motorcycle_id"], name: "index_bookings_on_motorcycle_id"
+#     t.index ["user_id"], name: "index_bookings_on_user_id"
