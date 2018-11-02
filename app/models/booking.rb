2ad7ebbb14239abbbdd7e9d1cdddd57eb87883c0
Anders Fleck <andersfleck@gmail.com>
2018-11-02 15:01:02 +0100

    geocoding

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,10 @@ class Booking < ApplicationRecord
   belongs_to :user
   has_many :reviews
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
+
   def self.user_bookings(user)
   	Booking.where(user: user)
   end
