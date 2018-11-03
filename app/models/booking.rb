7333c8b837beb7ba9f70035c9521258f29db9088
Anders Fleck <andersfleck@gmail.com>
2018-11-03 17:31:58 +0100

    ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :motorcycle
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, :dependent => :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
